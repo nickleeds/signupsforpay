aa676d18a3b16dc06951199b7f647b48e1c41786
nickleeds <34907766+nickleeds@users.noreply.github.com>
2020-03-18 12:28:52 -0700

    add

--- js/blog-bundle.js
+++ js/blog-bundle.js
@@ -88,7 +88,7 @@ function init_page(){
 
 // function to find all the blog posts
 function search_blog() {
-	var files = ["2020_02_03_Binance_US", "2020_02_03_N26","2020_02_03_Sofi_Money","2020_02_05_Webull", "2020_02_07_NextSeed","2020_02_07_Stash","2020_02_07_GroundFloor", "2020_03_17_Nvstr", "2020_03_17_Robinhood","2020_02_11_Firstrade", "2020_02_11_Public", "2020_03_17_Crypto", "default.jpg"];
+	var files = ["2020_02_03_Binance_US", "2020_02_03_N26","2020_02_03_Sofi_Money","2020_02_05_Webull", "2020_02_07_NextSeed","2020_02_07_Stash","2020_02_07_GroundFloor", "2020_03_17_Nvstr", "2020_03_17_Dough", "2020_03_17_Robinhood","2020_02_11_Firstrade", "2020_02_11_Public", "2020_03_17_Crypto", "default.jpg"];
 
 	for(var i in files) {
 		if(path.extname(files[i]) === "") {
